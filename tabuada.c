a38b7e197e826d6e325acc15dfb117ffe619855e
[alisson] <[alisson_araujo76@outlook.com]>
2020-11-19 19:59:16 -0300

    just another test

--- tabuada.c
+++ tabuada.c
@@ -7,13 +7,13 @@ int vnum=0;
 int main() {
     printf("\nPrograma que calcula a tabuada do número digitado\n");
     while(true){
-        printf("\nentre com o número desejado\n");
+        printf("\nEntre com o número desejado\n");
         scanf("\n%i", &vnum);
         printf("\n");
         for (int i=1; i<=10; i++){
             printf("%i X %i = %i\n", vnum, i, (vnum*i));
         }
-        printf("\ndeseja continuar?\n");
+        printf("\nDeseja continuar?\n");
         printf("\ns/n?\n");
         scanf("\n%c", &vdeseja);
         if (vdeseja == 's' || vdeseja == 'S'){
